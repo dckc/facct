0a441dbacd73b9c8cdea414d59946e9da2b12705
Dan Connolly <http://www.madmode.com/>
2014-11-12 22:10:48 -0600

    refactor cliAccess   - datatype for Problem

--- src/Main.hs
+++ src/Main.hs
@@ -7,35 +7,52 @@ import qualified Gnome.Keyring as GK
 
 main :: IO ()
 main = do
-    args <- Sys.getArgs
-    run $ cli args
+    maybeAccess <- cliAccess Sys.getArgs GK.findNetworkPassword
+    report maybeAccess
     where
-        run (Right dbname) = do
-            creds <- findCreds GK.findNetworkPassword dbname
-            explore creds
-            where
-                explore (Right (c : _cs)) = print (GK.networkUser $ GK.networkPasswordNetwork c,
-                                                    GK.networkPasswordSecret c)
-                explore (Right []) = lose "no matching passwords"
-                explore (Left oops) = lose oops
-        run (Left oops) = lose oops
-        lose oops = do
-            hPrint stderr oops
+        report (Right creds) = print creds
+        report (Left oops) = do
+            hPrint stderr $ show oops
             exitFailure
 
+data Problem = Usage String
+    | NoUsername
+    | NoMatchingPasswords String
+    | KeyRingError GK.KeyringError
+  deriving Show
 
-cli :: [String] -> Either String String
+cliAccess :: (IO [String]) -> (GK.Network -> GK.Operation [GK.NetworkPassword]) -> IO (Either Problem (String, String))
+cliAccess getArgs find = do
+    args <- getArgs
+    findCreds $ cli args
+
+    where
+
+    findCreds :: (Either Problem String) -> IO (Either Problem (String, String))
+    findCreds (Right dbname) = do
+        answers <- GK.sync $ find $ dbloc dbname
+        return $ check answers
+        where
+
+        check :: Either GK.KeyringError [GK.NetworkPassword] -> Either Problem (String, String)
+        check (Right (c : _cs)) = checkUser $ GK.networkUser $ GK.networkPasswordNetwork c
+            where
+            checkUser (Just who) = Right (who, GK.networkPasswordSecret c)
+            checkUser Nothing = Left NoUsername
+        check (Right []) = Left $ NoMatchingPasswords dbname
+        check (Left oops) = Left $ KeyRingError oops
+    findCreds (Left oops) = return $ Left oops
+
+
+cli :: [String] -> Either Problem String
 cli [db] = Right db
-cli _ = Left "usage: prog dbname"
+cli _ = Left $ Usage "usage: prog dbname"
 
-
-findCreds find db = GK.sync $ find loc
-    where loc = GK.network {
-     GK.networkProtocol = Just "mysql",
-     GK.networkServer = Just "localhost",
-     GK.networkObject = Just db
-     }
-
+dbloc dbname = GK.network {
+            GK.networkProtocol = Just "mysql",
+            GK.networkServer = Just "localhost",
+            GK.networkObject = Just dbname
+        }
 
 --	GLib.setApplicationName "GNOME Keyring example"
 --	GLib.setProgramName "/path/to/binary"
